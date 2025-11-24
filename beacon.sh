43ca53592425356f8aac774b4e05d52a8dd746ae
Bonny <patrick@patrickelser.net>
2025-11-24 14:28:28 -0500

    Updated docker

---
+++ beacon.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/bash
+
+set -e
+
+export LLVM_COMPILER=clang
+BEACON="/beacon"
+cfile="";
+targetline="0";
+outfile="";
+insfile="";
+datadir=".";
+
+ordvar="--cfile";
+setvar="";
+
+for arg in "$@"; do
+	if [[ $arg == "-"* ]]
+	then
+		setvar=$arg;
+	elif [[ accept ]]
+	then
+		nextvar=""
+		if [[ $setvar != "" ]]
+		then
+			nextvar=$setvar;
+			setvar="";
+		else
+			if [[ $ordvar == "--cfile" ]]
+			then
+				nextvar="--cfile"
+				ordvar="--targetline"
+			elif [[ $ordvar == "--targetline" ]]
+			then
+				nextvar="--targetline"
+				ordvar=""
+			else
+				echo "Too many inputs"
+				exit
+			fi
+		fi
+		if [[ $nextvar == "-c" || $nextvar == "--cfile" ]] 
+		then
+			cfile=$arg;
+		elif [[ $nextvar == "-t" || $nextvar == "--targetline" ]]
+		then
+			targetline=$arg
+		elif [[ $nextvar == "-o" || $nextvar == "--outfile" ]]
+		then
+			outfile=$arg;
+		elif [[ $nextvar == "-i" || $nextvar == "--insfile" ]]
+		then
+			insfile=$arg;
+		elif [[ $nextvar == "-d" || $nextvar == "--datadir" ]]
+		then
+			datadir=$arg;
+		else
+			echo "Unrecognized option";
+		fi
+	fi
+done
+
+if [[ ! -v BEACON ]] 
+then
+	echo "Set Beacon dir"
+fi
+
+if [[ $cfile == "" ]] 
+then
+	echo "Requires cfile";
+	exit
+fi
+
+if [[ $outfile == "" ]]
+then
+	outfile=${cfile##*"/"}
+	outfile=${outfile%%".c"*}
+	if [[ $outfile == $cfile ]]
+	then
+		outfile="$outfile-out";
+	fi
+fi
+
+if [[ $insfile == "" ]]
+then
+	if [[ $outfile == *"-out" ]]
+	then
+		insfile="${outfile%%"-out"}-ins"
+	else
+		insfile="$outfile-ins";
+	fi
+fi
+
+if [[ $datadir != *"/" ]]
+then
+	datadir="$datadir/"
+fi
+
+ls $datadir 1>/dev/null 2>/dev/null|| mkdir $datadir;
+currentdir=$(pwd)
+
+wllvm -g $cfile -o $outfile
+extract-bc $outfile -o "$datadir$outfile.bc"
+echo "${cfile##*"/"}:$targetline" > "$datadir""line.txt"
+
+cd $datadir
+$BEACON/precondInfer/build/bin/precondInfer "$outfile.bc" --target-file=line.txt --join-bound=5
+$BEACON/Ins/build/Ins -output="$insfile.bc" -blocks=bbreaches.txt -afl -log=log.txt -load=range_res.txt ./transed.bc
+cd $currentdir
+clang "$datadir$insfile.bc" -o $insfile -lm -lz $BEACON/Fuzzer/afl-llvm-rt.o
+
